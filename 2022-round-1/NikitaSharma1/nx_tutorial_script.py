df4ab955850c2509e458f99f5df311e21e0b16ca
NikitaSharma1 <nikita07192001@gmail.com>
2022-03-30 14:34:21 +0530

    Jupyter Notebook

--- 2022-round-1/NikitaSharma1/nx_tutorial_script.py
+++ 2022-round-1/NikitaSharma1/nx_tutorial_script.py
@@ -10,31 +10,30 @@ import networkx as nx
 '''
 Declaration of Directed Graph
 '''
-NetworkX=nx.DiGraph()
+G=nx.DiGraph()
 
 # Adding edges will automatically declare the nodes
 
-NetworkX.add_edge(5,6)
-NetworkX.add_edge(1,(10,11))
-NetworkX.add_edge("spam",5)
-NetworkX.add_edge((10,11),1)
-NetworkX.add_edge("test",6)
-NetworkX.add_edge(5,1)
-NetworkX.add_edge(1.5,"test")
-NetworkX.add_edge("spam",(10,11))
+G.add_edge(5,6)
+G.add_edge(1,(10,11))
+G.add_edge("spam",5)
+G.add_edge((10,11),1)
+G.add_edge("test",6)
+G.add_edge(5,1)
+G.add_edge(1.5,"test")
+G.add_edge("spam",(10,11))
 
 
 '''
 Finding shortest path
 '''
-Shortest_Path= nx.shortest_path(NetworkX)
-
+Shortest_Path= nx.shortest_path(G)
+print(Shortest_Path)
 
 '''
 Plotting the directed graph
 '''
-layout=nx.planar_layout(NetworkX)
+layout=nx.planar_layout(G)
 
-nx.draw(NetworkX,pos=layout,arrows=True,with_labels=True,node_size=2600,node_color='#4ABF8C',
+nx.draw(G,pos=layout,arrows=True,with_labels=True,node_size=2600,node_color='#4ABF8C',
         node_shape='8',width=1.5)
-
