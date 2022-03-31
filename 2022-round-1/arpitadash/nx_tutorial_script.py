944266945881867d92cff68cce9238824e2ab027
arpitadash <arpitadash1998@gmail.com>
2022-03-31 12:01:30 +0530

    Create Python script of digraph
    Create Digraph, plot the graph, and find shortest path between all nodes

---
+++ 2022-round-1/arpitadash/nx_tutorial_script.py
@@ -0,0 +1,27 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+#Creating a networkX digraph
+G=nx.DiGraph()
+#Adding nodes of int, str and tuple type
+G.add_node(1)
+G.add_node("string1")
+G.add_node((3,2))
+G.add_node('string2')
+G.add_nodes_from([2,3,10])
+G.add_nodes_from([(4,{'color':'red'})])
+#Adding edges between the nodes
+G.add_edges_from([(1,2),(1,3),(1,'string1'),(2,3),('string2',1),(1,'string2'),(1,(3,2)),(2,10),((3,2),'string2'),('string2',4)])
+# print(G.number_of_nodes())
+# print(G.number_of_edges())
+G_node_list=G.nodes
+p=nx.shortest_path(G)
+print('Printing shortest path')
+for index,node in enumerate(G_node_list):
+    print('From',node)
+    for index2,node2 in enumerate(G_node_list):
+        if index!=index2 and nx.has_path(G, node, node2):
+            print('To', node2, ':', p[node][node2])
+nx.draw(G,pos=nx.circular_layout(G),with_labels=True)
+plt.show()
+
