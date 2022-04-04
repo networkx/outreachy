9ac835985911a69d73def21c61b0ba14fd0e0a01
joyce <joyceannie111@gmail.com>
2022-04-04 12:46:56 -0700

    Updated code

--- 2022-round-1/joyceannie/nx_tutorial_script.py
+++ 2022-round-1/joyceannie/nx_tutorial_script.py
@@ -6,15 +6,22 @@ import matplotlib.pyplot as plt
 DG = nx.DiGraph()
 
 #adding nodes and edges
+#adding nodes and edges
 DG.add_edge(1, 2)
-DG.add_edge('foo', 'bar')
-DG.add_edge(1, ('a', 'b'))
+DG.add_edge(2, ('foo', 'bar'))
+DG.add_edge(4, 'a')
+DG.add_edge('a', 3)
+DG.add_edge(('foo', 'bar'), 3)
+DG.add_edge(4, 2)
+DG.add_edge(4, 1)
 DG.add_edge(3, 4)
-DG.add_edge(('a', 'b'), 3)
-DG.add_edge(('foo', 'bar'), ('a', 'b'))
 
 # find the shortest path between all pairs of nodes
-sp = nx.shortest_path(DG)
+shortest_path = nx.shortest_path(DG)
+#printing shortest paths
+for source_node in shortest_path.keys():
+    for dest_node in shortest_path[source_node]:
+        print(f'Shortest path from node {source_node} to node {dest_node}: {shortest_path[source_node][dest_node]}')
 
 #plot the graph
 nx.draw(DG, with_labels=True, font_weight='bold')
