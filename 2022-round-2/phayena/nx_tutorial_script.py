8e716360356902be7a0fc5dbc294d4320cc4a35c
phayena <mariamfayena@gmail.com>
2022-10-12 10:43:01 +0100

    Creating a tutorial python script

--- 2022-round-2/phayena/nx_tutorial_script.py
+++ 2022-round-2/phayena/nx_tutorial_script.py
@@ -1,5 +1,4 @@
 # import the library
-from email.charset import SHORTEST
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -10,25 +9,26 @@ G = nx.DiGraph()
 G.add_node(2)
 G.add_node(3)
 
+
 # adding a string type node
 G.add_node("boy")
-G.add_node("girl")
+
 
 # adding a list type node
 G.add_nodes_from([20,25])
 
 #adding a tuple type node
-G.add_nodes_from([(5,{4:"father"}),(6,{7:"mother"})])
+G.add_nodes_from([(5,{4:"father"})])
 
 #growing G by adding edges
 G.add_edge(2,3)
 G.add_edge(3,2)
-G.add_edge("girl",2)
 G.add_edge(3,"boy")
-G.add_edges_from("girl")
+G.add_edges_from("boy")
 G.add_edges_from(2,[20,25])
-G.add_edges_from([[20,25],(5,{4:"father"})])
-G.add_edges_from([(6,{7:"mother"}),3])
+G.add_edges_from(3,[20,25],"boy")
+G.add_edges_from([20,25],(5,{4:"father"}))
+G.add_edges_from((5,{4:"father"}),2)
 
 #print number of nodes
 print(G.number_of_nodes())
@@ -42,7 +42,8 @@ shortest_path_g=nx.shortest_path(G)
 print(shortest_path_g)
 
 #drawing the Graph
-nx.draw(G, with_labels = True)
+nx.draw(G, with_labels = True,font_weight='bold')
+plt.title("Direction of graph")
 #showing the graph
 plt.show()
 
