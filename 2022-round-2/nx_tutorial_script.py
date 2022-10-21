463bb4a667f655e563edfa936626b2e22deaa941
dnabanita7 <dashnabanita@gmail.com>
2022-10-21 10:49:42 +0530

    nx tutorial script

---
+++ 2022-round-2/nx_tutorial_script.py
@@ -0,0 +1,21 @@
+using networkx as nx
+
+# Create a DiGraph
+G = nx.DiGraph()
+
+# Add nodes to the graph
+nodes = [7, "Nabanita", ("ABCD", 56.8))
+G.add_nodes_from(nodes)
+
+# Add edges between the nodes
+edges = [ (7, "Nabanita"), ("Nabanita", ("ABCD", 56.8)), (("ABCD", 56.8), 7),
+	("Nabanita", 7) ]
+G.add_edges_from(edges)
+
+# Find shortest path b/w all pairs of nodes
+paths = dict(nx.all_pairs_shortest_path(G))
+for path in paths:
+	print(path, paths[key])
+
+# Plot the graph
+nx.draw(G, with_labels=True, font_weight="bold")
