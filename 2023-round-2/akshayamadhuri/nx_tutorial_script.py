02c3806a2fb2411cd044e8a598ddc9a78587c087
AKSHAYA MADHURI <76612327+akshayamadhuri@users.noreply.github.com>
2023-10-04 17:55:33 +0530

    Update nx_tutorial_script.py

--- 2023-round-2/akshayamadhuri/nx_tutorial_script.py
+++ 2023-round-2/akshayamadhuri/nx_tutorial_script.py
@@ -4,26 +4,32 @@ import matplotlib.pyplot as plt
 # Create a directed graph object
 G = nx.DiGraph()
 
-# Add nodes of different types 
+# Add nodes of different types (int, str, tuple)
 G.add_node(1)
-G.add_node("Adam")
-G.add_node((2, 3))
+G.add_node("aam")
+G.add_node((3, 4))
 
 # Add multiple edges between nodes
-G.add_edge(1, "Adam")
-G.add_edge("Adam", (2, 3))
-G.add_edge((2, 3), 1)
+G.add_edge(1, "aam")
+G.add_edge("aam", (3, 4))
+G.add_edge((3, 4), 4)
 
-# Add more nodes and edges 
-G.add_nodes_from([4, "Bob", (4, 5), "Charlie", (6, 7), 8])
-G.add_edges_from([(4, "Bob"), ("B", (4, 5)), (1, "Charlie"), ((6, 7), 8)])
+# Add more nodes and edges (up to 10 nodes)
+G.add_edges_from([('a', 'b'),('aam', 1),(9, (3, 4)), (9, 1),
+    ('aam', 9),
+    ((3, 4), 0),
+    ('a', 9),
+    (0, 'b')])
 
 # Find and print the shortest path between all pairs of nodes
 for node1 in G.nodes():
     for node2 in G.nodes():
         if node1 != node2:
-            shortest_path = nx.shortest_path(G, source=node1, target=node2)
-            print(f"Shortest path from {node1} to {node2}: {shortest_path}")
+            if nx.has_path(G, source=node1, target=node2):
+                shortest_path = nx.shortest_path(G, source=node1, target=node2)
+                print(f"Shortest path from {node1} to {node2}: {shortest_path}")
+            else:
+                print(f"No path from {node1} to {node2}")
 
 # Plot the graph
 pos = nx.spring_layout(G, seed=42)  # Positioning nodes for visualization
